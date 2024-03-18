bc06eb5f4b292e374464bd6820d9754cdac706e6
Aravind <aravindmunna240@gmail.com>
2024-03-19 01:40:19 +0530

    Reversed string

--- src/basicQuestions/ReverseANumber.java
+++ src/basicQuestions/ReverseANumber.java
@@ -1,2 +1,30 @@
-package basicQuestions;public class ReverseANumber {
+package basicQuestions;
+
+import java.util.Scanner;
+
+public class ReverseANumber {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+
+        int rev = 0;
+
+//        while (num != 0){
+//            rev = rev * 10 + num% 10;
+//            num = num / 10;
+//        }
+//        System.out.println(rev);
+
+//        Using string builder
+
+        StringBuilder sb = new StringBuilder(String.valueOf(num));
+        StringBuilder reversed =  sb.reverse();
+        System.out.println(reversed);
+
+//        Can use StringBuffer class and the method is same as that of StringBuilder
+    }
 }
--- src/basicQuestions/ReverseStringValue.java
+++ src/basicQuestions/ReverseStringValue.java
@@ -1,2 +1,19 @@
-package basicQuestions;public class ReverseStringValue {
+package basicQuestions;
+
+public class ReverseStringValue {
+
+
+    public static void main(String[] args) {
+
+        String str = "ABCD";
+        int length = str.length();
+        String rev = " ";
+//        for (int i = length-1;i >=0 ; i--){
+//            rev = rev+str.charAt(i);
+//        }
+//        System.out.println(rev);
+
+        StringBuilder stringBuilder = new StringBuilder(str);
+        System.out.println(stringBuilder.reverse());
+    }
 }
