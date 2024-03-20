4152754c2c0467536039e3d3bd514de3e1456142
Aravind <aravindmunna240@gmail.com>
2024-03-21 01:07:56 +0530

    Fibanocci series

---
+++ src/basicQuestions/FibanocciSeries.java
@@ -0,0 +1,18 @@
+package basicQuestions;
+
+public class FibanocciSeries {
+
+    public static void main(String[] args) {
+
+        int n1 = 0 , n2 = 1 ,sum = 0;
+        System.out.print(n1 + " " + n2 + " ");
+
+        for (int i = 2 ; i < 10 ;i++){
+            sum = n1 + n2;
+            System.out.print(sum+ " " );
+            n1 = n2;
+            n2 = sum;
+
+        }
+    }
+}
