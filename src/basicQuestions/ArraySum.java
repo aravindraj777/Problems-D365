21656c62f40a4a747a51527edea6f75f91b35da3
Aravind <aravindmunna240@gmail.com>
2024-03-22 00:24:54 +0530

    sum of array by stream

---
+++ src/basicQuestions/ArraySum.java
@@ -0,0 +1,14 @@
+package basicQuestions;
+
+import java.util.Arrays;
+
+public class ArraySum {
+
+    public static void main(String[] args) {
+
+        int[] num = {1,2,3,4,5};
+
+        int sum = Arrays.stream(num).sum();
+        System.out.println(sum);
+    }
+}
