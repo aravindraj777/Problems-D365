222073519ab57340403d42b1808b0c16d39859f4
Aravind <aravindmunna240@gmail.com>
2024-03-31 10:24:36 +0530

    day-five

---
+++ src/basicQuestions/AddDigits.java
@@ -0,0 +1,21 @@
+package basicQuestions;
+
+import java.util.Scanner;
+
+public class AddDigits {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = sc.nextInt();
+
+        int sum = 0;
+        while (number > 0) {
+
+            int extNum = number % 10;
+            sum = sum + extNum;
+            number = number / 10;
+        }
+    }
+}
