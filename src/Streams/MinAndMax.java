9c1c17ceac334147c28b9208ac4f77b809de53aa
Aravind <aravindmunna240@gmail.com>
2024-07-05 18:57:39 +0530

    stream operations:sort ,min and max

--- src/Streams/MinAndMax.java
+++ src/Streams/MinAndMax.java
@@ -1,2 +1,41 @@
-package Streams;public class MinAndMax {
+package Streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class MinAndMax {
+
+
+    public static void main(String[] args) {
+
+
+        List<Integer> numbersList = Arrays.asList(10,2,3,5,6,7,78,8);
+        long countOfEven = numbersList.stream().filter(num -> num % 2 == 0).count();
+        System.out.println(countOfEven);
+
+
+//        min
+       Optional<Integer> minValue =  numbersList.stream().min((val1, val2) -> {
+            return val1.compareTo(val2);
+        });
+
+        System.out.println(minValue.get());
+
+//        max
+        Optional<Integer> maxValue = numbersList.stream().max((val1,val2) -> {
+            return val1.compareTo(val2);
+        });
+        System.out.println(maxValue.get());
+
+
+//        Reduce
+
+        List<String> stringList  = Arrays.asList("A","B","C","D","E","F");
+        Optional<String> reduced = stringList.stream().reduce((val,combinedVal) -> {
+            return val+combinedVal;
+        });
+
+        System.out.println(reduced.get());
+    }
 }
--- src/Streams/Sorted.java
+++ src/Streams/Sorted.java
@@ -1,2 +1,25 @@
-package Streams;public class Sorted {
+package Streams;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sorted {
+
+    public static void main(String[] args) {
+
+        List<Integer> list1 = Arrays.asList(2,4,3,2,6,90,45,74,32);
+        List<Integer> sortedList = list1.stream().sorted().collect(Collectors.toList());
+        System.out.println(sortedList);
+        List<Integer> reverseOrder = list1.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println(reverseOrder);
+
+        List<String> list2 = Arrays.asList("A","G","K","X","W","U");
+        List<String> stringList = list2.stream().sorted().collect(Collectors.toList());
+        System.out.println(stringList);
+
+        List<String> reverseStringList = list2.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        System.out.println(reverseStringList);
+    }
 }
