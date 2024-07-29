97ae74da5ffbcafa65c67965f126a20a1d9556ab
Aravind <aravindmunna240@gmail.com>
2024-07-30 01:57:22 +0530

    Merge sort : correction

--- src/sortings/merge/MergeNewTwo.java
+++ src/sortings/merge/MergeNewTwo.java
@@ -2,30 +2,29 @@ package sortings.merge;
 
 import java.util.Arrays;
 
-public class MergeFive {
+public class MergeNewTwo {
 
     public static void main(String[] args) {
-
-        int[] arr = {78,12,32,23,10};
-        arr = mergeSort(arr);
+        int[] arr = {45,12,32,56,10};
+        arr  = mergeSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static int[] mergeSort(int[] arr){
+    private static int[] mergeSort(int[] arr){
 
         if (arr.length == 1){
             return arr;
         }
-        int midIndex = arr.length / 2;
-        int[] left = mergeSort(Arrays.copyOfRange(arr,0,midIndex));
-        int[] right = mergeSort(Arrays.copyOfRange(arr,midIndex,arr.length));
-        return mergeHelper(left,right);
+
+        int mid = arr.length / 2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
     }
 
-    public static int[] mergeHelper(int[] left, int[] right){
-
-        int[] combined = new int[left.length+right.length];
-        int index = 0;
+    private static int[] merge(int[] left, int[] right){
+        int[] combined = new int[left.length + right.length];
+        int index = 0 ;
         int i = 0;
         int j = 0;
 
@@ -41,40 +40,18 @@ public class MergeFive {
                 j++;
             }
         }
-        while (j < right.length){
-            combined[index] = right[j];
-            index ++ ;
-            j++;
-        }
+
         while (i < left.length){
             combined[index] = left[i];
             index++;
             i++;
         }
+
+        while (j < right.length){
+            combined[index] = right[j];
+            index++;
+            j++;
+        }
         return combined;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
