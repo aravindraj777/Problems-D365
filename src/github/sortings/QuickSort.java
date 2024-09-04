830d6386e6d9b3739470c093ec0b4ed33c4ac4ad
Aravind <aravindmunna240@gmail.com>
2024-09-04 20:38:00 +0530

    Sortings: Quick sort

---
+++ src/github/sortings/QuickSort.java
@@ -0,0 +1,43 @@
+package github.sortings;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {32,12,45,90,12,2};
+        quickSort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void quickSort(int[] arr, int first, int last){
+
+        if (first >= last){
+            return ;
+        }
+
+        int middle = (first + last) /2;
+        int f = first;
+        int l = last;
+
+        int pivot = arr[middle];
+        while (f <= l){
+            while (arr[f] < pivot){
+                f++;
+            }
+            while (arr[l] > pivot){
+                l--;
+            }
+            if (f <= l){
+                int temp = arr[f];
+                arr[f] = arr[l];
+                arr[l] = temp;
+                f++;
+                l--;
+            }
+        }
+        quickSort(arr,first,l);
+        quickSort(arr,f,last);
+    }
+}
