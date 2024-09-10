57f8bb6785f3c95b88f70000810625d4d43c8d04
Aravind <aravindmunna240@gmail.com>
2024-09-10 12:41:46 +0530

    Divide and conquer : Find the specific target from the array in o(logn) comp

---
+++ src/github/divideAndConquer/SearchTheTarget.java
@@ -0,0 +1,34 @@
+package github.divideAndConquer;
+
+public class SearchTheTarget {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int target = 3;
+        int place = search(arr,target);
+        System.out.println("The target is in index "+ place);
+    }
+
+    private static int search(int[] arr, int target){
+
+        int left = 0;
+        int right = arr.length - 1;
+
+
+        while (left < right){
+            int mid = left+ (right - left)/2;
+
+            if (arr[mid] < target){
+                left = mid + 1;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                return mid;
+            }
+
+        }
+
+        return -1;
+    }
+}
