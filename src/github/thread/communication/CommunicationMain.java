442bb8aff811898d51c07a8287212981dc9d806d
Aravind <aravindmunna240@gmail.com>
2024-09-15 18:56:45 +0530

    Thread communication , making the producer thread wait for 30s and then activate the consumer thread first

--- src/github/thread/communication/CommunicationMain.java
+++ src/github/thread/communication/CommunicationMain.java
@@ -1,2 +1,24 @@
-package github.thread.communication;public class CommunicationMain {
+package github.thread.communication;
+
+public class CommunicationMain {
+
+    public static void main(String[] args) {
+
+        SharedResource sharedResourceObj = new SharedResource();
+
+        Thread producerThread = new Thread(()->{
+            try {
+                Thread.sleep(3000);
+            }
+            catch (Exception e){
+                System.out.println("Exceptions in main");
+            }
+            sharedResourceObj.addItem();
+        });
+        Thread consumerThread = new Thread(sharedResourceObj::consumeItem);
+
+        producerThread.start();
+        consumerThread.start();
+
+     }
 }
