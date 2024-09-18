7365903593139b74aef8d718510c14600d72da5c
Aravind <aravindmunna240@gmail.com>
2024-09-18 20:49:54 +0530

    Read write lock : shared resource , a producer and consumer method for read and write lock

--- src/github/thread/locks/readWriteLock/SharedResource.java
+++ src/github/thread/locks/readWriteLock/SharedResource.java
@@ -1,2 +1,39 @@
-package github.thread.locks.readWriteLock;public class SharedResource {
+package github.thread.locks.readWriteLock;
+
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class SharedResource {
+
+    boolean isAvailable = false;
+
+    public void producer(ReadWriteLock lock){
+        try {
+            lock.readLock().lock();
+            System.out.println("Read lock acquired by "+ Thread.currentThread().getName());
+            Thread.sleep(8000);
+        }
+        catch (Exception e){
+//        Exceptions
+        }
+        finally {
+            lock.readLock().unlock();
+            System.out.println("Read lock released by "+ Thread.currentThread().getName());
+        }
+    }
+
+    public void consume(ReadWriteLock lock){
+        try {
+            lock.writeLock().lock();
+            System.out.println("Write lock acquired by "+Thread.currentThread().getName());
+            isAvailable = false;
+        }
+
+        catch (Exception e){
+//            Consume exceptions
+        }
+        finally {
+            lock.writeLock().unlock();
+            System.out.println("Write lock released by"+Thread.currentThread().getName());
+        }
+    }
 }
