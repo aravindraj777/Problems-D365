4446ab42cbfc81b7d4ed7bc62cd88955271c356b
Aravind <aravindmunna240@gmail.com>
2024-11-17 18:39:57 +0530

    Wildcard differences

--- src/github/generics/boundedGenerics/wildCards/Print.java
+++ src/github/generics/boundedGenerics/wildCards/Print.java
@@ -7,4 +7,13 @@ public class Print {
     public void setPrintValues(List<? super Vehicle> vehicleList){
 
     }
+
+
+    public void computeList(List<? extends Number> source, List<? extends Number> destination){
+
+    }
+
+    public <T extends Number> void computeList1(List<T> source, List<T> destination){
+
+    }
 }
--- src/github/generics/boundedGenerics/wildCards/WildCardMain.java
+++ src/github/generics/boundedGenerics/wildCards/WildCardMain.java
@@ -15,5 +15,17 @@ public class WildCardMain {
         Print printObj = new Print();
 //        Vehicle list is acceptable as it is lower bound happening hereg
         printObj.setPrintValues(vehicleList);
+
+//        For wild card method
+        List<Integer> integerList = new ArrayList<>();
+        List<Float> floatList = new ArrayList<>();
+        List<Integer> integerList1 = new ArrayList<>();
+
+//        This is acceptable in wildcard , we can add multiple datatypes also
+        printObj.computeList(integerList,floatList);
+
+//        This is not acceptable as it is not a wildcard method .
+//        This is acceptable as both having same datatypes.g
+        printObj.computeList1(integerList,integerList1);
     }
 }
