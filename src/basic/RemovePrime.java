efbed49e075f66d41d9d56436b0b169c7f964c2f
Aravind <aravindmunna240@gmail.com>
2024-11-18 19:46:29 +0530

    Remove prime

--- src/basic/RemovePrime.java
+++ src/basic/RemovePrime.java
@@ -1,2 +1,30 @@
-package basic;public class RemovePrime {
+package basic;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemovePrime {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,4,5,1,3,6,10};
+        System.out.println(Arrays.toString(nonPrime(arr)));
+
+
+    }
+
+    private static  int[] nonPrime(int[] prime){
+
+        Set<Integer> nonPrime = new HashSet<>();
+       for (int i = 2 ;i < prime.length ;i++){
+           if (prime[i] % i != 0){
+               nonPrime.add(prime[i]);
+           }
+           else {
+               continue;
+           }
+       }
+       return nonPrime.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
