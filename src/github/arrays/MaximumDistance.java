79ccb0fb720f14f5887387524789b0dbe94b3460
Aravind <aravindmunna240@gmail.com>
2024-11-20 23:03:52 +0530

    max distance

--- src/github/arrays/MaximumDistance.java
+++ src/github/arrays/MaximumDistance.java
@@ -1,2 +1,22 @@
-package github.arrays;public class MaximumDistance {
-}
+//package github.arrays;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class MaximumDistance {
+//
+//    public static void main(String[] args) {
+//
+//        ArrayList<Integer> arrays = {{1,2,3},{4,5},{1,2,3}};
+//        maxDistance()
+//    }
+//
+//    public static int maxDistance(List<List<Integer>> arrays) {
+//
+//        for (int i = 0 ; i < arrays.toArray().length ; i++){
+//            for (int  arr : arrays.get(i)){
+//                System.out.println(arr);
+//            }
+//        }
+//    }
+//}
