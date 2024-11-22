b4a2f377df262a1e69249e842eccf8b0186e936e
Aravind <aravindmunna240@gmail.com>
2024-11-22 18:41:16 +0530

    Hashtable QProbing

--- src/hashtable/QuadraticProbing.java
+++ src/hashtable/QuadraticProbing.java
@@ -1,2 +1,33 @@
-package hashtable;public class QuadraticProbing {
-}
+//package hashtable;
+//
+//public class QuadraticProbing {
+//
+//    private Node[] bucket;
+//    private int size;
+//    QuadraticProbing(int size){
+//        this.size = size;
+//    }
+//   class Node {
+//
+//       String key;
+//       String value;
+//       Node next;
+//
+//       Node (String key, String value){
+//           this.key = key;
+//           this.value = value;
+//       }
+//    }
+//
+//    public void set(String key, String value){
+//
+//    }
+//
+//    private int hashFunction(String key){
+//        char[] ch = key.toCharArray();
+//        int index = 0;
+//        for (char c : ch){
+//            index = (index + c) % size;
+//        }
+//    }
+//}
