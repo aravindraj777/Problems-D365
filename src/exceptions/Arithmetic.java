c095b372f623f25c201257f604cebd4903907d10
Aravind <aravindmunna240@gmail.com>
2024-12-19 22:58:41 +0530

    Exception

---
+++ src/exceptions/Arithmetic.java
@@ -0,0 +1,25 @@
+package exceptions;
+
+public class Arithmetic {
+
+
+
+
+
+
+
+        public static void main(String[] args) {
+
+
+            try {
+                int val = 5/0;
+                System.out.println(val);
+            }
+            catch (ArithmeticException e){
+                System.out.println("Cannot divide number by zero"+e.getMessage());
+            }
+
+        }
+
+
+}
