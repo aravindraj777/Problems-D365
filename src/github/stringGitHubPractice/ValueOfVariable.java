0c8439574207a9afb0ede179e3743a93755d414d
Aravind <aravindmunna240@gmail.com>
2024-12-20 22:24:02 +0530

    Value of variable

--- src/github/stringGitHubPractice/ValueOfVariable.java
+++ src/github/stringGitHubPractice/ValueOfVariable.java
@@ -1,2 +1,24 @@
-package github.stringGitHubPractice;public class ValueOfVariable {
+package github.stringGitHubPractice;
+
+public class ValueOfVariable {
+
+    public static void main(String[] args) {
+
+        String[] operations = {"--X","X++","X++"};
+        System.out.println(finalValueAfterOperations(operations));
+    }
+
+    public static int finalValueAfterOperations(String[] operations) {
+
+        int x = 0;
+        for (String s : operations){
+            if (s == "--X" || s == "X--"){
+                x = x - 1;
+            }
+            else {
+                x = x + 1;
+            }
+        }
+        return x;
+    }
 }
