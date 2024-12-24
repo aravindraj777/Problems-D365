435208b6ac39d0dc9b9d9174945cbf824a24feb3
Aravind <aravindmunna240@gmail.com>
2024-12-24 07:09:44 +0530

    Smallest missing  : DVC

--- src/github/divideAndConquer/FindTheSmallestMissing.java
+++ src/github/divideAndConquer/FindTheSmallestMissing.java
@@ -1,2 +1,14 @@
-package github.divideAndConquer;public class FindTheSmallestMissing {
+package github.divideAndConquer;
+
+public class FindTheSmallestMissing {
+
+    public static void main(String[] args) {
+
+        String str = "abc2345fg";
+        for (char ch : str.toCharArray()){
+            int hash = ch;
+            System.out.println(hash+ ":"+ch);
+
+        }
+    }
 }
