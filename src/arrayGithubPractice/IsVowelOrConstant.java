a79419c8b05b4c73abfac6f3da109086c14e7d54
Aravind <aravindmunna240@gmail.com>
2024-12-25 09:14:59 +0530

    Vowel or not  :ARRAY WK

--- src/arrayGithubPractice/IsVowelOrConstant.java
+++ src/arrayGithubPractice/IsVowelOrConstant.java
@@ -1,2 +1,22 @@
-package arrayGithubPractice;public class IsVowelOrConstant {
+package arrayGithubPractice;
+
+import java.util.Arrays;
+
+public class IsVowelOrConstant {
+
+    public static void main(String[] args) {
+
+        char ch = 'b';
+        boolean isVowel = isVowel(ch);
+        System.out.println(ch + " is vowel " + isVowel);
+
+
+    }
+
+    private static boolean isVowel(char word) {
+       String vowels = "aeiouAEIOU";
+       return vowels.indexOf(word) != -1;
+    }
+
+
 }
