7468df7ed1e4d1c5f5cc5185dfa6a81f8786ebbb
Aravind <aravindmunna240@gmail.com>
2024-12-28 23:03:40 +0530

    Sum of digits

--- src/basic/SumOfDigits.java
+++ src/basic/SumOfDigits.java
@@ -1,2 +1,18 @@
-package basic;public class SumOfDigits {
+package basic;
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int sum = 0;
+
+        while (num > 0){
+            int lastNum = num % 10;
+            sum = sum + lastNum;
+            num = num / 10;
+        }
+
+        System.out.println(sum);
+    }
 }
