c62961d687fe82c3386055e4a5f6b2084b8544c8
Aravind <aravindmunna240@gmail.com>
2024-12-31 21:48:19 +0530

    Lemonade - leetcode

--- src/github/arrays/LemonadeChange.java
+++ src/github/arrays/LemonadeChange.java
@@ -1,2 +1,22 @@
-package github.arrays;public class LemonadeChange {
-}
+//package github.arrays;
+//
+//public class LemonadeChange {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    private static boolean lemonadeChange(int[] bills){
+//
+//        int change = 0;
+//        boolean flag = false;
+//        for (int i = 0 ; i < bills.length ; i++){
+//            if (bills[i] - 5 == 0){
+//                flag = true;
+//            } else if (bills[i] - change ) {
+//                Thread t = new Thread();
+//
+//            }
+//        }
+//    }
+//}
