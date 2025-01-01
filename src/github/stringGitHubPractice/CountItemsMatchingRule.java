7c60efb875c56227327d4679d6af47172836c954
Aravind <aravindmunna240@gmail.com>
2025-01-01 22:31:37 +0530

    Matching rule - problem

--- src/github/stringGitHubPractice/CountItemsMatchingRule.java
+++ src/github/stringGitHubPractice/CountItemsMatchingRule.java
@@ -1,23 +1,23 @@
-//package github.stringGitHubPractice;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//public class CountItemsMatchingRule {
-//
-//    public static void main(String[] args) {
-//
-//        List<List<String>> items = List.of(
-//                List.of("phone", "blue", "pixel"),
-//                List.of("computer", "silver", "lenovo"),
-//                List.of("phone", "gold", "iphone")
-//        );
-//
-//    }
-//    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-//
-//
-//
-//    }
-//}
+package github.stringGitHubPractice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CountItemsMatchingRule {
+
+    public static void main(String[] args) {
+
+        List<List<String>> items = List.of(
+                List.of("phone", "blue", "pixel"),
+                List.of("computer", "silver", "lenovo"),
+                List.of("phone", "gold", "iphone")
+        );
+
+    }
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+
+
+    }
+}
