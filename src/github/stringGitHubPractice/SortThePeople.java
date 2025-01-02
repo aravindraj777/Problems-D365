236d334024e81a9557efe1b5d522e3c48f456c8c
Aravind <aravindmunna240@gmail.com>
2025-01-02 22:59:11 +0530

    Sort the people  - leet

--- src/github/stringGitHubPractice/SortThePeople.java
+++ src/github/stringGitHubPractice/SortThePeople.java
@@ -1,33 +1,33 @@
-//package github.stringGitHubPractice;
-//
-//import java.util.Arrays;
-//import java.util.Comparator;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//public class SortThePeople {
-//
-//    public static void main(String[] args) {
-//
-//        String[] names = {"Mary","John","Emma"};
-//        int[] heights = {180,165,170};
-//
-//        sortPeople(names,heights);
-//    }
-//
-//    public static String[] sortPeople(String[] names, int[] heights) {
-//
-//        List<>
-//        for(int i = 0 ; i < names.length ; i++){
-//
-//            for (int j = 0 ; j < heights.length ; j++){
-//
-//            }
-//        }
-//       List<Integer> heightsSorted =  Arrays.stream(heights).boxed().sorted(Comparator.reverseOrder()).toList();
-//       List<String> names =
-//        System.out.println(heightsSorted);
-//        return names;
-//    };
-//
-//}
+package github.stringGitHubPractice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortThePeople {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Mary","John","Emma"};
+        int[] heights = {180,165,170};
+
+        sortPeople(names,heights);
+    }
+
+    public static String[] sortPeople(String[] names, int[] heights) {
+
+        List<>
+        for(int i = 0 ; i < names.length ; i++){
+
+            for (int j = 0 ; j < heights.length ; j++){
+
+            }
+        }
+       List<Integer> heightsSorted =  Arrays.stream(heights).boxed().sorted(Comparator.reverseOrder()).toList();
+       List<String> names =
+        System.out.println(heightsSorted);
+        return names;
+    };
+
+}
