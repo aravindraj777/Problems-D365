5d0ccac8fac615005e175b1da93ab652a5586a1e
Aravind <aravindmunna240@gmail.com>
2025-01-21 22:48:56 +0530

    Rev :Bubble sort

---
+++ src/github/sortings/BubbleSort.java
@@ -0,0 +1,27 @@
+package github.sortings;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] nonSorted = {3,1,56,23,10,30};
+        bubbleSort(nonSorted);
+        System.out.println("Sorted array : "+ Arrays.toString(nonSorted));
+    }
+
+    private static void bubbleSort(int[] arr){
+
+        int temp;
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = i + 1 ; j < arr.length  ; j++){
+                if (arr[i] > arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
