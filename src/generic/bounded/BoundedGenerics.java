78a573b0a5fb571744e12086dc75469e655f010b
Aravind <aravindmunna240@gmail.com>
2025-03-09 21:39:09 +0530

    Generics : Check with reflection api

--- src/generic/bounded/BoundedGenerics.java
+++ src/generic/bounded/BoundedGenerics.java
@@ -1,14 +1,23 @@
 package generic.bounded;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
 public class BoundedGenerics<T extends Animal> {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException {
             Dogs dogs = new Dogs();
             Cats cats = new Cats();
 
             BoundedGenerics<Animal> dogsBoundedGenerics = new BoundedGenerics<>();
             dogsBoundedGenerics.displayAnimal(dogs);
             dogsBoundedGenerics.displayAnimal(cats);
+
+        Method method = BoundedGenerics.class.getMethod("displayAnimal", Animal.class);
+        System.out.println("Method signature"+method);
+
+        Type superClass  = BoundedGenerics.class.getGenericSuperclass();
+        System.out.println("Superclass Type" + superClass);
 
     }
 
